0719ff1748aa005ec9a89be3989ec381d4515140
David Hernández <d.hdza19@gmail.com>
2022-12-04 05:55:54 -0600

    add deleting bookstores feature

--- app/javascript/components/Bookstore.jsx
+++ app/javascript/components/Bookstore.jsx
@@ -19,11 +19,70 @@ const Bookstore = () => {
           .catch(() => navigate("/bookstores"));
     }, [params.id]);
 
-    const addHtmlEntities = (str) => {
-        return String(str).replace(/&lt;/g, "<").replace(/&gt;/g, ">");
+    const bookList = () => {
+      let bookList = "No books available";
+  
+      if (bookstore.books.length > 0) {
+        bookList = bookstore.books
+          .split(",")
+          .map((book, index) => (
+            <li key={index} className="list-group-item">
+              {book}
+            </li>
+          ));
+      }
+  
+      return ingredientList;
+    };
+
+    const deleteBookstore = () => {
+      const url = `/api/v1/destroy/${params.id}`;
+      const token = document.querySelector('meta[name="csrf-token"]').content;
+  
+      fetch(url, {
+        method: "DELETE",
+        headers: {
+          "X-CSRF-Token": token,
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
+          throw new Error("Network response was not ok.");
+        })
+        .then(() => navigate("/bookstores"))
+        .catch((error) => console.log(error.message));
     };
 
     // TODO: Render bookstore with its list of books
+    return (
+      <div className="">
+        <div className="hero position-relative d-flex align-items-center justify-content-center">
+          <img
+            src={bookstore.image}
+            alt={`${bookstore.codename} image`}
+            className="img-fluid position-absolute"
+          />
+          <div className="overlay bg-dark position-absolute" />
+          <h1 className="display-4 position-relative text-white">
+            {bookstore.codename}
+          </h1>
+        </div>
+        <div className="container py-5">
+          <div className="col-sm-12 col-lg-2">
+              <button
+                type="button"
+                className="btn btn-danger"
+                onClick={deleteBookstore}
+              >
+                Delete Bookstore
+              </button>
+            </div>
+        </div>
+      </div>
+    );
 };
   
 export default Bookstore;
--- app/javascript/routes/index.jsx
+++ app/javascript/routes/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Bookstore from "../components/Bookstore";
 import Bookstores from "../components/Bookstores";
 import Home from "../components/Home";
 import NewBookstore from "../components/NewBookstore";
@@ -9,6 +10,7 @@ export default (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/bookstores" element={<Bookstores />} />
+      <Route path="/bookstore/:id" element={<Bookstore />} />
       <Route path="/bookstore" element={<NewBookstore />} />
     </Routes>
   </Router>

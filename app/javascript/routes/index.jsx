3f413c3175d73ec3628c8b7a025918f341d71745
David Hernández <d.hdza19@gmail.com>
2022-12-04 02:00:24 -0600

    add functionality for creating bookstores

--- app/javascript/routes/index.jsx
+++ app/javascript/routes/index.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Bookstores from "../components/Bookstores";
 import Home from "../components/Home";
+import NewBookstore from "../components/NewBookstore";
 
 export default (
   <Router>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/bookstores" element={<Bookstores />} />
+      <Route path="/bookstore" element={<NewBookstore />} />
     </Routes>
   </Router>
 );
